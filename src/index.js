a4c080bb58df101cb97ec722eb9b6528937294ae
Tilda Udufo <mathildaudufo@gmail.com>
2021-01-12 11:58:54 +0100

    Todos can now be added to projects

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import hamburgerMenu from './hamburger.js'
 import { ProjectUI } from './projectUI.js'
 import { addProject, projectEditForm, editProject } from './project.js'
+import { addTodo } from './todo.js'
 
 function getProjects() {
     return JSON.parse(localStorage.getItem("allProjects") || "[]");
@@ -18,6 +19,7 @@ function init() {
 function addListeners() {
     document.querySelector('#new-proj-form').addEventListener('submit', addProject);
     document.querySelector('#edit-proj-form').addEventListener('submit', editProject);
+    document.querySelector('#new-task-form').addEventListener('submit', addTodo);
     $('#editProj').on('shown.bs.modal', projectEditForm);
 }
 init();
--- src/todo.js
+++ src/todo.js
@@ -1,9 +1,19 @@
 import { ProjectUI } from './projectUI.js'
 import { getProjects } from './index.js'
-const Todo = function(name, description, priority, dueDate) {
-    return { name, description, priority, dueDate }
+const Todo = function(name, description, priority, dueDate, completed) {
+    return { name, description, priority, dueDate, completed }
 }
-const addTodo = () => {
+
+const addTodo = event => {
+    event.preventDefault();
+    const name = document.querySelector('.new-task-name').value;
+    const desc = document.querySelector('.new-task-desc').value;
+    const priority = document.querySelector('.new-task-pri').value;
+    const dueDate = document.querySelector('.new-task-date').value;
+    $('#newTask').modal('hide');
+    let allProjects = getProjects();
     const index = ProjectUI.getActiveProject().projectIndex;
-    getProjects()[index].lists.shift();
+    allProjects[index].lists.push(Todo(name, desc, priority, dueDate, false));
+    localStorage.setItem('allProjects', JSON.stringify(allProjects));
 }
+export { addTodo }
