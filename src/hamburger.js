510f9fcd7452f025b3414a19a9614ca61ebe06b4
Tilda Udufo <mathildaudufo@gmail.com>
2021-01-14 11:10:59 +0100

    Used prettier

--- src/hamburger.js
+++ src/hamburger.js
@@ -1,17 +1,14 @@
-function hamburgerMenu() {
-    const ham = document.querySelector('#hamburgers');
-    ham.addEventListener('change', function() {
-        const hamitems = document.querySelector('.hamitems-div');
-        if (ham.checked) {
-            hamitems.classList.add('active');
-            hamitems.classList.remove('hidden');
-        } else {
-            hamitems.classList.add('hidden');
-            hamitems.classList.remove('active');
-        }
-    });
-
-
-
-}
-export default hamburgerMenu
+const hamburgerMenu = () => {
+	const ham = document.querySelector("#hamburgers");
+	ham.addEventListener("change", function () {
+		const hamitems = document.querySelector(".hamitems-div");
+		if (ham.checked) {
+			hamitems.classList.add("active");
+			hamitems.classList.remove("hidden");
+		} else {
+			hamitems.classList.add("hidden");
+			hamitems.classList.remove("active");
+		}
+	});
+};
+export default hamburgerMenu;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
-import hamburgerMenu from './hamburger.js'
-import { ProjectUI } from './projectUI.js'
-import { addProject, projectEditForm } from './project.js'
-import defaultProject from './defaultProject'
+import hamburgerMenu from "./hamburger.js";
+import { ProjectUI } from "./projectUI.js";
+import { addProject, projectEditForm } from "./project.js";
+import defaultProject from "./defaultProject";
 
-function getProjects() {
-    return JSON.parse(localStorage.getItem("allProjects") || "[]");
+const getProjects = () => {
+	return JSON.parse(localStorage.getItem("allProjects") || "[]");
 }
 
-function init() {
-    hamburgerMenu();
-    addListeners();
-    ProjectUI.addAllProjectsToUI();
-    let hamitems = document.querySelector('.hamitems');
-    let firstProjectListTag = hamitems.firstElementChild;
-    if (firstProjectListTag) { firstProjectListTag.firstElementChild.click() }; //goes to the first project page on document load
+const init = () => {
+	hamburgerMenu();
+	addListeners();
+	ProjectUI.addAllProjectsToUI();
+	let hamitems = document.querySelector(".hamitems");
+	let firstProjectListTag = hamitems.firstElementChild;
+
+	//goes to the first project page on document load
+	if (firstProjectListTag) {
+		firstProjectListTag.firstElementChild.click();
+	}
 }
 
-function addListeners() {
-    document.querySelector('#new-proj-form').addEventListener('submit', addProject);
-    $('#editProj').on('shown.bs.modal', projectEditForm);
+const addListeners = () => {
+	document
+		.querySelector("#new-proj-form")
+		.addEventListener("submit", addProject);
+	$("#editProj").on("shown.bs.modal", projectEditForm);
 }
 
 window.onload = defaultProject();
 init();
-export { getProjects }
+export { getProjects };
